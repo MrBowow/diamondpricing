ee82a624d4791b58fc3f6e97ab326be51b557f5c
Mr. Bow wow <47439665+MrBowow@users.noreply.github.com>
2022-02-17 11:59:28 +0800

    Update diam.py

--- diam.py
+++ diam.py
@@ -8,7 +8,7 @@ sns.set_theme(style="whitegrid")
 
 
 diamonddata = pd.read_csv("diamonds_regression.csv")
-diamdata = diamonddata.drop(columns = ['Unnamed: 0', 'x','y','z'])
+diamdata = diamonddata.drop(columns = ['Unnamed: 0'])
 
 if st.sidebar.checkbox('Show DataFrame'):
        st.write(diamdata.head(20))
